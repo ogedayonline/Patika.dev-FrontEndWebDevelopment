fdbc7ed2426148ad0a4cec8676704af33078456f
Ogeday Yalcin <ogeday12@gmail.com>
2021-11-03 14:45:41 +0300

    ShoppingListApp add

--- JS-Tasks/alisverisListesi/app.js
+++ JS-Tasks/alisverisListesi/app.js
@@ -1,13 +1,29 @@
 let inputDOM = document.getElementById("input");
 let buttonDOM = document.getElementById("button");
 let listDOM = document.getElementById("liste");
-let listeElemani2 = document.getElementsByTagName("LI");
+let containerDOM = document.getElementById("container");
 
 buttonDOM.addEventListener("click", function () {
   let listeElemani = document.createElement("li");
+  let deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteButton");
+  deleteButton.setAttribute("type", "button");
+  deleteButton.innerText = "Delete";
   listeElemani.innerText = inputDOM.value;
-  listDOM.appendChild(listeElemani);
-  inputDOM.value = "";
+  if (listeElemani.innerText == "") {
+    var element = document.getElementById("myToast");
+    var myToast = new bootstrap.Toast(element, {
+      delay: 5000,
+    });
+    myToast.show();
+  } else {
+    listeElemani.appendChild(deleteButton);
+    listDOM.appendChild(listeElemani);
+    inputDOM.value = "";
+  }
+  deleteButton.addEventListener("click", function () {
+    listDOM.removeChild(listeElemani);
+  });
 });
 
 listDOM.addEventListener(
