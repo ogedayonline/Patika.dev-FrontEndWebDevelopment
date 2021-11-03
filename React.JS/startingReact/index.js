299722f7ff20fcc6328cef26f17cd1967c715532
Ogeday Yalcin <ogeday12@gmail.com>
2021-11-03 15:55:42 +0300

    React.js module adding

---
+++ React.JS/startingReact/index.js
@@ -0,0 +1,9 @@
+import { user, text, topla, cikart } from "./myModule.js";
+
+console.log(topla(5000000000, 20000000000));
+
+console.log(cikart(1000000, 1));
+
+console.log(text);
+
+console.log(user);
---
+++ React.JS/startingReact/myModule.js
@@ -0,0 +1,17 @@
+const cikart = (a, b) => {
+  return a - b;
+};
+
+function topla(a, b) {
+  return a + b;
+}
+
+const text = "text";
+
+const user = {
+  name: "Ogeday",
+  surname: "Yalcin",
+  age: 20,
+};
+
+export { topla, cikart, text, user };
